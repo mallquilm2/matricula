e570fdddea040bc0a40f0857157244dcb13ba85b
miguel <mallquilm@gmail.com>
2024-12-02 22:47:14 -0500

    configuración properties inicial

---
+++ src/main/java/edu/cibertec/matricula/HolaMundo.java
@@ -0,0 +1,11 @@
+
+package edu.cibertec.matricula;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HolaMundo {
+    public void saludar(){
+        System.err.println("Saludando desde el servicio");
+    }
+}
--- src/main/java/edu/cibertec/matricula/MatriculaApplication.java
+++ src/main/java/edu/cibertec/matricula/MatriculaApplication.java
@@ -1,13 +1,38 @@
 package edu.cibertec.matricula;
 
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class MatriculaApplication {
+public class MatriculaApplication implements CommandLineRunner{
+    
+    @Autowired
+    HolaMundo holaMundo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MatriculaApplication.class, args);
 	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        holaMundo.saludar();
+    }
+    
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+        return args -> {
+                System.out.println("Inspeccionando los beans creados por spring boot.");
+                String[] beanNames = ctx.getBeanDefinitionNames();
+                Arrays.sort(beanNames);
+                for (String beanName : beanNames) {
+                    System.out.println(beanName);
+                }
+        };
+    }
 
 }
