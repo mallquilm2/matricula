56ae8ddddfd98270ecb9c15576fb0db93b17110a
miguel <mallquilm@gmail.com>
2024-12-30 21:22:41 -0500

    Ejemplo con mapstruct

--- src/main/java/edu/cibertec/matricula/MatriculaApplication.java
+++ src/main/java/edu/cibertec/matricula/MatriculaApplication.java
@@ -1,5 +1,8 @@
 package edu.cibertec.matricula;
 
+import edu.cibertec.matricula.dao.entity.UsuarioEntity;
+import edu.cibertec.matricula.domain.UsuarioDomain;
+import edu.cibertec.matricula.mapper.UsuarioMapper;
 import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -15,7 +18,20 @@ public class MatriculaApplication {
     HolaMundo holaMundo;
 
 	public static void main(String[] args) {
-		SpringApplication.run(MatriculaApplication.class, args);
+            
+            UsuarioDomain usuario = new UsuarioDomain();
+            usuario.setId("1");
+            usuario.setContrasenia("12345");
+            usuario.setNombre("Juan Perez");
+            
+            UsuarioEntity usuarioEntity = UsuarioMapper.INSTANCE
+                    .usuarioDomainToUsuarioEntity(usuario);
+            
+            System.out.println("ID: "+usuarioEntity.getUsuario());
+            System.out.println("NOMBRE: "+usuarioEntity.getNombreCompleto());
+            System.out.println("CONTRASEÑA: "+usuarioEntity.getClave());
+            
+            SpringApplication.run(MatriculaApplication.class, args);
 	}
 
 }
---
+++ src/main/java/edu/cibertec/matricula/domain/UsuarioDomain.java
@@ -0,0 +1,14 @@
+
+package edu.cibertec.matricula.domain;
+
+import lombok.Data;
+
+
+@Data
+public class UsuarioDomain {
+    
+    private String id;
+    private String contrasenia;
+    private String nombre;
+    
+}
